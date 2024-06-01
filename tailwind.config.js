1afe90c848803d5b1e8babc684dbc351fbf2be1a
micheledinelli <dinellimichele00@gmail.com>
2024-06-01 22:58:10 +0200

    Modified assets path and added README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ export default {
         noto: ["Noto Sans Mono", "sans-serif"],
       },
       backgroundImage: {
-        "hero-pattern-1": "url('/src/assets/mesh-gradient-1.png')",
-        "hero-pattern-2": "url('/src/assets/mesh-gradient-2.png')",
-        "hero-pattern-3": "url('/src/assets/mesh-gradient-3.png')",
+        "hero-pattern-1": "url('/mesh-gradient-1.png')",
+        "hero-pattern-2": "url('/mesh-gradient-2.png')",
+        "hero-pattern-3": "url('/mesh-gradient-3.png')",
       },
     },
   },
