24146d3a5d12c0816a38ece53ff8b1789bd7a704
micheledinelli <dinellimichele00@gmail.com>
2024-06-03 01:26:47 +0200

    Changed development port

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
     strictPort: true,
   },
   server: {
-    port: 3000,
+    port: 8080,
     strictPort: true,
     host: true,
-    origin: "http://localhost:3000",
+    origin: "http://localhost:8080",
     proxy: {
       "/api": {
         target: "http://localhost:8888",
